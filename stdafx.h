3a16ea5d069c6974497fe8c87047d9015ce2a378
FyiurAmron <spamove@gmail.com>
2017-01-23 04:43:41 +0100

    disabled MSVC-specific code

--- stdafx.h
+++ stdafx.h
@@ -9,7 +9,7 @@
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' \
 version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-#include "targetver.h"
+//#include "targetver.h"
 
 //#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
